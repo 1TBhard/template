49b0b218782e7bcee67dbd6cd23a87aa89e2194e
1TBhard <was1383@naver.com>
2022-12-31 03:22:47 +0900

    public import 예제 추가

--- src/@types/import-jpg.d.ts
+++ src/@types/import-jpg.d.ts
@@ -1,3 +1,5 @@
+// *.jpg 파일에 대한 선언
+// 이 부분이 있어야 jpg 파일을 import 할때 ts error 발생하지 않음
 declare module "*.jpg" {
 	const content: string;
 	export default content;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { Test } from "src/testFolder/Test";
+import forest from "public/imgs/forest.jpg";
 
 export const App = () => {
+	useEffect(() => {
+		const myImg = new Image();
+		myImg.src = forest;
+		window.document.body.appendChild(myImg);
+	}, []);
+
 	return (
 		<div>
 			<h1>App</h1>
